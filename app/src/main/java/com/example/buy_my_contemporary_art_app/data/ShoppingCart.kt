a9e1830d6109aa77bc163085a7e71d59986a5a77
Almaz Ermilov <almaz.ermilov@gmail.com>
2024-03-30 23:25:33 +0100

    upd shopping bag func

---
+++ app/src/main/java/com/example/buy_my_contemporary_art_app/data/ShoppingCart.kt
@@ -0,0 +1,10 @@
+package com.example.buy_my_contemporary_art_app.model
+
+package com.example.buy_my_contemporary_art_app.data
+
+data class ShoppingCartItem(
+    val id: Int,
+    val name: String,
+    val frameInfo: String,
+    val price: Float
+)
---
+++ app/src/main/java/com/example/buy_my_contemporary_art_app/data/ShoppingCartItem.kt
@@ -0,0 +1,4 @@
+package com.example.buy_my_contemporary_art_app.data
+
+class ShoppingCartItem {
+}
