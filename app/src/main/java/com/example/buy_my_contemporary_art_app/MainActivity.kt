054eda8592ff9b2f2bc11ce5f5b3e7e5c297643b
Almaz Ermilov <almaz.ermilov@gmail.com>
2024-04-07 20:31:50 +0200

    upd photos screens

--- app/src/main/java/com/example/buy_my_contemporary_art_app/MainActivity.kt
+++ app/src/main/java/com/example/buy_my_contemporary_art_app/MainActivity.kt
@@ -1,18 +1,18 @@
 package com.example.buy_my_contemporary_art_app
 
+//import android.annotation.SuppressLint
+
+//import androidx.compose.foundation.layout.FlowColumnScopeInstance.align
+
+
+//import com.example.buy_my_contemporary_art_app.data.*
+//import com.example.buy_my_contemporary_art_app.data.ShoppingCartDataSource
+//import com.example.buy_my_contemporary_art_app.ui.HomeScreen
+//import com.example.buy_my_contemporary_art_app.ui.ShoppingCartViewModel
+
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-//import android.annotation.SuppressLint
-
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.items
-
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
-
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -20,60 +20,69 @@ import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-//import androidx.compose.foundation.layout.FlowColumnScopeInstance.align
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BlurOn
+import androidx.compose.material.icons.filled.Fastfood
 import androidx.compose.material.icons.filled.Pets
 import androidx.compose.material.icons.filled.SportsSoccer
-import androidx.compose.material.icons.filled.Fastfood
-import androidx.compose.material.icons.filled.BlurOn
-import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.ListItem
-import androidx.compose.runtime.*
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
-
-
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-
-//import com.example.buy_my_contemporary_art_app.data.*
-//import com.example.buy_my_contemporary_art_app.data.ShoppingCartDataSource
-//import com.example.buy_my_contemporary_art_app.ui.HomeScreen
-//import com.example.buy_my_contemporary_art_app.ui.ShoppingCartViewModel
-import com.example.buy_my_contemporary_art_app.ui.theme.BuyMyContemporaryArtAppTheme
-
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.buy_my_contemporary_art_app.ui.theme.BuyMyContemporaryArtAppTheme
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -495,7 +504,7 @@ fun PhotosScreen(artistId: Long, navController: NavController) {
                     })
 
                     Text(
-                        "Photos to buy",
+                        "Photos",
                         textAlign = TextAlign.Center,
                         modifier = Modifier
                             .fillMaxWidth()
@@ -540,6 +549,7 @@ fun PhotoCard(photo: Photo, navController: NavController) {
             Text(
                 text = photo.title,
                 modifier = Modifier.padding(8.dp)
+                    .align(Alignment.CenterHorizontally)
             )
             // Add more details or actions for each photo card here
         }
@@ -571,7 +581,7 @@ fun PhotosByCategoryScreen(category: Category, navController: NavController) {
                     })
 
                     Text(
-                        "Photos to buy",
+                        "Photos",
                         textAlign = TextAlign.Center,
                         modifier = Modifier
                             .fillMaxWidth()
